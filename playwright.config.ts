b761490fea51e703d78579cf35d039bc2116da86
Victor Shan <victorshanvs@gmail.com>
2024-03-06 21:55:11 -0800

    add basic front-page tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,11 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        headless: !!process.env.CI || !!process.env.PLAYWRIGHT_HEADLESS,
+        video: "retain-on-failure",
+      },
     },
 
     {
@@ -69,9 +73,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run dev",
+    // url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+    port: 3000,
+  },
 });
